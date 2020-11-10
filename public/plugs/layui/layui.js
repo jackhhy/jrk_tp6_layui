109f7dc815db9c6bff297d3361b20c5b7402a25e
JackGemini <hehuiyi2@163.com>
2020-11-10 08:57:51 +0800

    解决IE浏览器assign问题

--- public/plugs/layui/layui.js
+++ public/plugs/layui/layui.js
@@ -1,5 +1,31 @@
 /** layui-v2.5.6 MIT License By https://www.layui.com */
-;!function (e) {
+//解决iE浏览器兼容问题
+if(!Object.assign){
+    if (typeof Object.assign !== 'function') {
+        Object.assign = function(target) {
+            'use strict';
+            if (target === null || target === undefined) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+            target = Object(target);
+            for (var index = 1; index < arguments.length; index++) {
+                var source = arguments[index];
+                if (source === undefined || source === null ) {
+                    continue;
+                }
+                if (source != null) {
+                    for (var key in source) {
+                        if (Object.prototype.hasOwnProperty.call(source, key)) {
+                            target[key] = source[key];
+                        }
+                    }
+                }
+            }
+            return target;
+        };
+    }
+}
+!function (e) {
     "use strict";
     var jrkmodules = {
         "okTab": "jrkmodules/okTab",
